c5dc450b99c1ec926157396edc858b097c9d677b
adriesco <144839808+adriesco@users.noreply.github.com>
2023-11-10 18:52:35 +0100

    ejercicios

---
+++ StringCharEnum/src/StringCharEnum/ejercicio10.java
@@ -0,0 +1,27 @@
+package StringCharEnum;
+
+public class ejercicio10 {
+	enum ColorArcoiris {
+	       ROJO, NARANJA, AMARILLO, VERDE, AÑIL, AZUL, VIOLETA
+	   }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ColorArcoiris[] n = new ColorArcoiris[7];
+		
+		 n[0] = ColorArcoiris.ROJO;
+		 n[1] = ColorArcoiris.NARANJA;
+		 n[2] = ColorArcoiris.AMARILLO;
+		 n[3] = ColorArcoiris.VERDE;
+		 n[4] = ColorArcoiris.AÑIL;
+		 n[5] = ColorArcoiris.AZUL;
+		 n[6] = ColorArcoiris.VIOLETA;
+		
+		 System.out.println(n[0] + " " + n[1] + " " + n[2] + " " + n[3] + " " + n[4] + " " + n[5] + " " + n[6]);
+		
+		
+		
+		}
+							}

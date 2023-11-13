14ce21a0586f025d90a3a5599649bee37125d845
adriesco <144839808+adriesco@users.noreply.github.com>
2023-11-13 18:55:52 +0100

    Mas ejercicios

---
+++ Arrays/src/Array/ejercicio1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numeroentero[];
+		numeroentero = new int[5];
+		
+		numeroentero[0]= 1;
+		numeroentero[1]= 2;
+		numeroentero[2]= 3;
+		numeroentero[3]= 4;
+		numeroentero[4]= 5;
+		System.out.println(numeroentero[0] + numeroentero[1] + numeroentero[2] + numeroentero[3] + numeroentero[4]);
+	}
+			}
+
